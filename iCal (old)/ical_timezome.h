ecf3711c22ab2015d99890be14d361bd1b308f56
miyako <keisuke.miyako@4d.com>
2014-02-11 14:16:45 +0900

    removed old branch

--- iCal (old)/ical_timezome.h
+++
@@ -1,9 +0,0 @@
-#include "4DPluginAPI.h"
-
-#import <CalendarStore/CalendarStore.h>
-
-// --- Timezone
-void iCal_TIMEZONE_LIST(sLONG_PTR *pResult, PackagePtr pParams);
-void iCal_Get_timezone_info(sLONG_PTR *pResult, PackagePtr pParams);
-void iCal_Get_timezone_for_offset(sLONG_PTR *pResult, PackagePtr pParams);
-void iCal_Get_system_timezone(sLONG_PTR *pResult, PackagePtr pParams);
