d6ce1638878ec3774d5a0dc6bf5d47bbdf84ec68
miyako <keisuke.miyako@4d.com>
2018-11-05 12:17:50 +0900

    merge v2 to main

--- iCal/ical_calendar_store.h
+++
@@ -1,8 +0,0 @@
-#include "4DPluginAPI.h"
-
-#import <CalendarStore/CalendarStore.h>
-
-// --- Calendar Store
-void iCal_QUERY_EVENT(sLONG_PTR *pResult, PackagePtr pParams);
-void iCal_GET_CALENDAR_LIST(sLONG_PTR *pResult, PackagePtr pParams);
-void iCal_QUERY_TASK(sLONG_PTR *pResult, PackagePtr pParams);
